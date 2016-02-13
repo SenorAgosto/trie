6580eb323028f2ae348c7cf30504842ae3afdb68
Austin Gilbert <ceretullis@gmail.com>
2016-02-12 22:09:59 -0600

    Some missing stuff needed to compile.

---
+++ trie/src/Dummy.cpp
@@ -0,0 +1,10 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wmissing-variable-declarations"
+
+namespace trie { namespace fixed_size {
+
+    char DummyLnk4221;
+}}
+
+#pragma clang diagnostic pop
