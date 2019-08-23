917c103c274a4937a9b8e84ac6585cb671bd7961
Austin Gilbert <ceretullis@gmail.com>
2019-08-22 20:31:26 -0500

    Resolving conflicts from cherry-picking to sign commits

---
+++ _cmake/compilers/gcc49.cmake
@@ -0,0 +1,26 @@
+# ANALYSIS will be defined when using scan-build
+if(NOT DEFINED SCAN_BUILD)
+    add_definitions(
+            --std=c++11
+
+            -Wall
+            -Wextra 
+
+            -Wno-unknown-pragmas
+            -Wno-unused-local-typedefs
+            -Wno-unused-variable
+        )
+
+    if(${CMAKE_BUILD_TYPE} MATCHES "Debug") 
+        add_definitions(
+	    -g
+	    -O0
+        )
+    endif() 
+
+    if(${CMAKE_BUILD_TYPE} MATCHES "Release")
+        add_definitions(
+	    -O4
+        )
+    endif()
+endif()
